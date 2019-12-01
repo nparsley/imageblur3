2c37f1b415bc0430ad622635e9e3c7de4e3ced9e
Nick Parsley <nicholasparsley@gmail.com>
2019-12-01 05:52:10 +0000

    ImageBlur3

---
+++ imageblur3.rb
@@ -0,0 +1,89 @@
+#Calling a method within itslef (recursion) - use what have already previously built
+#instance variables -- look @ image to return 
+
+#IMAGE BLUR 3
+def blur_distance (distance)
+ #call the blur method for distance times (ex 2x)
+  distance.times do
+     blur ()
+   end
+end
+  #if element == 1
+    #return blur
+
+#@image.each do |data|
+
+
+
+
+
+
+
+#IMAGE BLUR 1-2
+class Image     
+  def initialize(array)  
+    @image = array   
+    @coordinates = []
+  end
+
+
+  def blur_distance (distance)
+ #call the blur method for distance times (ex 2x)
+  distance.times do
+     blur 
+   end
+  end
+
+  def output_image  
+    @image.each do |row|
+      puts row.join  
+    end
+  end
+
+  def blur
+      @image.each_with_index do |row, row_number|
+        row.each_with_index do |element, column_number|
+          if element == 1      
+            @coordinates.push([row_number, column_number])
+          end
+        end
+      end
+      puts @coordinates
+
+      @image.each_with_index do |row, row_number|
+        row.each_with_index do |element, column_number|
+            @coordinates.each do |x_coordinate, y_coordinate|
+              if row_number == x_coordinate && column_number == y_coordinate
+                @width = @image[0].count
+                @height = @image.count     
+                  @image [row_number +1][column_number] = 1 unless row_number >= @height -1  
+                  @image [row_number -1][column_number] = 1 unless row_number == 0
+                  @image [row_number][column_number +1] = 1 unless column_number >= @width -1 
+                  @image [row_number][column_number -1] = 1 unless column_number == 0 
+              end           
+            end
+        end 
+      end
+       @image.each do |data|
+        puts data.join    
+      end        
+    end
+  end
+
+
+
+image = Image.new([ 
+
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+])
+#image.blur
+image.blur_distance(2)
